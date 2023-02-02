5289242cf47402d0ad050a41dd047a0d778f3735
keyur6021 <86195043+keyur6021@users.noreply.github.com>
2023-02-02 23:31:35 +0530

    add react-bootstrap and navbar-component

---
+++ src/components/NavbarComponent.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { NavLink } from 'react-router-dom';
+
+
+const NavbarComponent = () => {
+
+    return (
+        <React.Fragment>
+            <Navbar bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <NavLink className={({ isActive }) => (isActive ? 'active' : 'inactive')} to="/">Home Page</NavLink>
+                        <NavLink className={({ isActive }) => (isActive ? 'active' : 'inactive')} to="/super-heroes">Super Heroes</NavLink>
+                        <NavLink className={({ isActive }) => (isActive ? 'active' : 'inactive')} to="/developers">React-query</NavLink>
+                    </Nav>
+                </Container>
+            </Navbar>
+        </React.Fragment >
+    )
+}
+
+export default NavbarComponent;
