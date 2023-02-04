bd471452bd4ff519046eeb53b6596ff2ce24613f
keyur6021 <86195043+keyur6021@users.noreply.github.com>
2023-02-05 00:28:26 +0530

    fetch the data of developer(first-api)

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarComponent from './components/NavbarComponent'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './components/HomePage';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <NavbarComponent />
         <Routes>
-          <Route exact path='/' element={<NavbarComponent />} />
+          <Route exact path='/' element={<HomePage />} />
         </Routes>
       </Router>
     </div>
