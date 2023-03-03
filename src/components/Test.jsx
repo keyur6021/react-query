46d77f68eb2e7f01edd8d4a408ebad96f8f21307
keyur6021 <mulanikeyur90@gmail.com>
2023-03-03 09:20:55 +0530

    add the useRef with form

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,27 +1,47 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 function Test() {
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
 
+    const formRef = useRef();
+
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log("test", firstName, lastName);
     }
+    const executeScroll = () => formRef.current.scrollIntoView();
 
     return (
         <React.Fragment>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    First Name:
-                    <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
-                </label>
-                <label>
-                    Last Name:
-                    <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} />
-                </label>
-                <button type="submit">Submit</button>
-            </form>
+            <button className='new-Button'>scroll button </button>
+            <div className='main-parent'>
+                <div ref={formRef} className='parent-Form'>
+                    <form onSubmit={handleSubmit}>
+                        <div className='main-form'>
+                            <div className='field'>
+                                <label>
+                                    First Name:
+                                </label>
+                                <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+                            </div>
+                            <div className='field'>
+                                <label>
+                                    Last Name:
+                                </label>
+                                <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+                            </div>
+                            <div className='parent-btn'>
+
+                                <button className='submit-btn' type="submit">Submit</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div>
+
+            </div>
         </React.Fragment>
     );
 }
